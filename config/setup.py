10ddae3121f3a6c9f95e9fd2e5378ab0b238b47e
Johandre van Deventer <johandrevdeventer@gmail.com>
2025-06-13 09:37:43 +0200

    add configuration module with project settings and constants

---
+++ config/setup.py
@@ -0,0 +1,8 @@
+"""
+Configuration settings for the project.
+Author: Johandré van Deventer
+Date: 2025-06-13
+"""
+
+# Configuration constants
+EXCEL_INPUT_FILE = "resources/device_details/device_ids.xlsx"
